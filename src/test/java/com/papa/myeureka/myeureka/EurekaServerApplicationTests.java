fe71e38239e68df3f87f849a209eca076e5e2740
parkerwu2 <982051137@qq.com>
2022-04-13 14:37:53 +0800

    Initial commit

---
+++ src/test/java/com/papa/myeureka/myeureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papa.myeureka.myeureka;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
